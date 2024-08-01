5173735fc891488729134041000e03f5b530e30c
MetaEMK <>
2024-08-01 23:43:50 +0200

    round values to 2 digits

--- src/components/CalcCostInfo.vue
+++ src/components/CalcCostInfo.vue
@@ -1,17 +1,17 @@
 <template>
-    <v-container v-if="perHourPrice > 0">
-        <h2>Kostenzusammensetzung: {{ roundedPerHourPrice }} € pro Stunde</h2>
+    <v-container v-if="perHourPrice">
+        <h2>Kostenzusammensetzung: {{ formatCurrency(perHourPrice) }} € pro Stunde</h2>
         <v-row>
             <v-col cols="12">
                 <v-list lines="one">
                     <v-list-item v-if="costStore.calcPerHourCosts > 0" title="Stundenkosten: "
                         :subtitle="formatCurrency(costStore.calcPerHourCosts) + ' €'">
                     </v-list-item>
-                    <v-list-item v-if="costStore.calcPerYearCosts > 0" title="Jahreskosten (anteilig):"
-                        :subtitle="formatCurrency(costStore.calcPerYearCosts) + ' €'">
+                    <v-list-item v-if="costStore.calcPerYearCosts! > 0" title="Jahreskosten (anteilig):"
+                        :subtitle="formatCurrency(costStore.calcPerYearCosts!) + ' €'">
                     </v-list-item>
-                    <v-list-item v-if="costStore.calcPlanePartCosts > 0" title="Flugzeugteile-Kosten"
-                        :subtitle="formatCurrency(costStore.calcPlanePartCosts) + ' €'">
+                    <v-list-item v-if="costStore.calcPlanePartCosts! > 0" title="Flugzeugteile-Kosten"
+                        :subtitle="formatCurrency(costStore.calcPlanePartCosts!) + ' €'">
                     </v-list-item>
                 </v-list>
             </v-col>
@@ -26,15 +26,15 @@ import { useCostStore } from '@/stores/cost.store';
 const costStore = useCostStore();
 
 const perHourPrice = computed(() => {
-    let costs = 0;
+    let costs: number = 0;
     if (costStore.calcPerHourCosts)
-        costs += costStore.calcPerHourCosts;
+        costs = costs + costStore.calcPerHourCosts
 
     if (costStore.calcPerYearCosts)
-        costs += costStore.calcPerYearCosts;
+        costs = costs + costStore.calcPerYearCosts
 
     if (costStore.calcPlanePartCosts)
-        costs += costStore.calcPlanePartCosts;
+        costs = costs + costStore.calcPerHourCosts
 
     return costs;
 });
@@ -46,8 +46,4 @@ const formatCurrency = (value: number) => {
         maximumFractionDigits: 2,
     }).format(value);
 };
-
-const roundedPerHourPrice = computed(() => {
-    return formatCurrency(perHourPrice.value);
-});
 </script>
